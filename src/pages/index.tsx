17ec874c3dc2465c90f76f9b0a77161e6ff522b4
Uwe Wohlleber <uwe@wohlleber.dev>
2026-01-27 17:13:58 +0100

    Update module resolution in tsconfig.json and adjust import paths in index.tsx for consistency and clarity.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
-import Header from '@site/src/components/header';
-import Hero from '@site/src/components/hero';
-import MySkills from '@site/src/components/my-skills';
-import Projects from '@site/src/components/projects';
-import Contact from '@site/src/components/contact';
-import Footer from '@site/src/components/footer';
-import ScrollToTop from '@site/src/components/scroll-to-top';
+import Header from '@site/src/components/header/index';
+import Hero from '@site/src/components/hero/index';
+import MySkills from '@site/src/components/my-skills/index';
+import Projects from '@site/src/components/projects/index';
+import Contact from '@site/src/components/contact/index';
+import Footer from '@site/src/components/footer/index';
+import ScrollToTop from '@site/src/components/scroll-to-top/index';
 import styles from './index.module.css';
 
 export default function Home(): JSX.Element {
