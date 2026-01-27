ceb7d8cd5160f4db5340d1b753d4e4a25bb6eb2d
Uwe Wohlleber <uwe@wohlleber.dev>
2026-01-27 17:16:25 +0100

    Refactor import paths in index.tsx for improved consistency and clarity.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
-import Header from '@site/src/components/header/index';
-import Hero from '@site/src/components/hero/index';
-import MySkills from '@site/src/components/my-skills/index';
-import Projects from '@site/src/components/projects/index';
-import Contact from '@site/src/components/contact/index';
-import Footer from '@site/src/components/footer/index';
-import ScrollToTop from '@site/src/components/scroll-to-top/index';
+import Header from '../components/header';
+import Hero from '../components/hero';
+import MySkills from '../components/my-skills';
+import Projects from '../components/projects';
+import Contact from '../components/contact';
+import Footer from '../components/footer';
+import ScrollToTop from '../components/scroll-to-top';
 import styles from './index.module.css';
 
 export default function Home(): JSX.Element {
